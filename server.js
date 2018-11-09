521efe63c2f8aef4893a01c30ee9db3b0dd63ad7
Martin MacDonald <martin.macdonald@screenmedia.co.uk>
2018-11-09 11:53:57 +0000

    again

--- server.js
+++ server.js
@@ -16,10 +16,9 @@ async function start() {
   const nuxt = new Nuxt(config);
 
   // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt);
-    await builder.build();
-  }
+
+  const builder = new Builder(nuxt);
+  await builder.build();
 
   // Give nuxt middleware to express
   app.use(nuxt.render);
